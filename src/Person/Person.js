dd7c1eae93d1ee6aff9788133f153405c0f91576
MarceloHeredia <marcelo.heredia@acad.pucrs.br>
2020-07-19 05:23:29 -0300

    hum

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,29 +1,13 @@
 import React from 'react';
-//import './Person.css';
-import styled from 'styled-components';
-
-const StyledDiv = styled.div`
-     width: 60%;
-     margin: 12px auto;
-     border-radius: 30px;
-     box-shadow:  6px 6px 7px #111111,
-                 -6px -6px 7px #2d2d2d;
-     padding: 16px;
-     text-align: center;
-
-     @media (min-width: 500px){
-         width: 450px;
-     }`;
-
+import classes from './Person.css';
 const person = (props) => {
 
     return (
-        //<div className="Person" style={style}>
-        <StyledDiv>
+        <div className={classes.Person} >
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
-        </StyledDiv>
+        </div>
     )
 };
 
