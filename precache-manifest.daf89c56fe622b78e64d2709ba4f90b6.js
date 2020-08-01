0340cae409ba49d26c9d206af66c2331e9f05cbe
MarceloHeredia <marcelo.heredia@acad.pucrs.br>
2020-08-01 14:05:17 -0300

    Updates

---
+++ precache-manifest.daf89c56fe622b78e64d2709ba4f90b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2544c7603c77259ab20f20aa3de56a19",
+    "url": "/react-training/index.html"
+  },
+  {
+    "revision": "47aca93ac1ed7050e94b",
+    "url": "/react-training/static/css/main.e05041af.chunk.css"
+  },
+  {
+    "revision": "8c4235d283ad91ca6267",
+    "url": "/react-training/static/js/2.49abf98c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-training/static/js/2.49abf98c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47aca93ac1ed7050e94b",
+    "url": "/react-training/static/js/main.6336bed6.chunk.js"
+  },
+  {
+    "revision": "6aa49adc647e8b171484",
+    "url": "/react-training/static/js/runtime-main.497f2de0.js"
+  }
+]);
